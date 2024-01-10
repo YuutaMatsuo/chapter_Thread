04dab407b2e1a601959fb103b97109ca98e63f93
user <user@192.168.1.106>
2024-01-10 15:13:28 +0900

    1/10 5限目補足

--- src/thread_test/Main.java
+++ src/thread_test/Main.java
@@ -4,19 +4,16 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Thread th = new Thread(new PrintThread());
+		PrintThread pt = new PrintThread();
+		Thread th = new Thread(pt);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("カウントダウンを開始します");
 		th.start();
-		
+
 		String cmd = sc.nextLine();
-		if(cmd.equals("stop")) {
-			try {
-			th.wait();
-			} catch(InterruptedException e) {
-				System.out.println("処理を一時停止します");
-			}
+		if (cmd.equals("stop")) {
+			pt.stopCount();
 		}
 	}
 }
--- src/thread_test/PrintThread.java
+++ src/thread_test/PrintThread.java
@@ -3,8 +3,14 @@ package thread_test;
 import java.util.concurrent.TimeUnit;
 
 public class PrintThread extends Thread {
+	
+	private boolean stop = false;
+	
 	public void run() {
 		for (int i = 30; i > 0; i--) {
+			if(stop) {
+				break;
+			}
 			System.out.print(i + "..");
 			try {
 				TimeUnit.SECONDS.sleep(1);
@@ -13,4 +19,8 @@ public class PrintThread extends Thread {
 			}
 		}
 	}
+	
+	// カウントダウン停止用のメソッドを作る(非推奨のtop()は使わない！）
+	public void stopCount() {
+		this.stop = true;	}
 }
